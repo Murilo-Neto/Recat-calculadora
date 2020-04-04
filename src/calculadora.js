360126da7084027fab61483ace8ab131dc425dd5
Murilo Neto <dev.muriloneto@gmail.com>
2020-04-04 16:41:48 -0300

    Ajuste para inicio de implementacao do estrutura HTML

---
+++ src/calculadora.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './calculadora.css';
+
+function Calculadora() {
+  return (
+    <h1>Calculadora</h1>
+  );
+}
+
+export default Calculadora;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculadora from './calculadora';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculadora />
   </React.StrictMode>,
   document.getElementById('root')
 );
